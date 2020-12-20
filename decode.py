fa31e91bf4bc5a2e07c0ff084fefe54417fd7395
legion-rgb <abhiram2001@hotmail.com>
2020-12-20 18:20:55 +0530

    changes done

--- decode.py
+++ decode.py
@@ -142,14 +142,31 @@ def baconian_decode(message):
 
 #affine
 def affine_decode(data,a,b):
-    MOD = 128
-    ans = []
-    for i in data:
-        for j in range(MOD):
-            if (a*j)%MOD==1:
-                a_inverse=j
-            ans.append(chr((a_inverse*(ord(i)+MOD-b))%MOD))
-    word = ''.join(ans)
-    return word
+    key = [a,b]
+    cipher = data
+    word=[]
+    def egcd(a, b):
+        x,y, u,v = 0,1, 1,0
+        while a != 0:
+            q, r = b//a, b%a
+            m, n = x-u*q, y-v*q
+            b,a, x,y, u,v = a,r, u,v, m,n
+        gcd = b
+        return gcd, x, y
 
-print(affine_decode('hello',2,2))
+    def modinv(a, m):
+        gcd, x, y = egcd(a, m)
+        if gcd != 1:
+            return None  # modular inverse does not exist
+        else:
+            return x % m
+    for c in cipher:
+        cipher = cipher.upper()
+        k = int(modinv(key[0], 26))*int((ord(c) - ord('A') - key[1]))
+        s = chr((k%26) +ord('A'))
+        word.append(s)
+        words = ''.join(word)
+    return words
+
+
+
